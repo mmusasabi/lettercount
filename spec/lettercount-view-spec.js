bccc0dbea085c7fbdad9a57b4d5fb3abd85a645a
KawamataKouhei <kawamata@basicinc.jp>
2017-01-20 17:38:14 +0900

    first commit

---
+++ spec/lettercount-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LettercountView from '../lib/lettercount-view';
+
+describe('LettercountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
